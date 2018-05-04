adb45e4ad5f2de3515958d9c117a13957dcf45df
rkemmy <rkemmy.rk69@gmail.com>
2018-05-04 11:19:38 +0300

    Create an index view function

--- gallery/pixie/views.py
+++ gallery/pixie/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def welcome(request):
-    return HttpResponse('Welcome to my photo gallery')
+def index(request):
+    return render(request, 'index.html')
