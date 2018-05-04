4c81b9f34f14e5bbc1ba66683187c3515d532fce
rkemmy <rkemmy.rk69@gmail.com>
2018-05-04 11:21:11 +0300

    Create location,images and category models

--- gallery/pixie/models.py
+++ gallery/pixie/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+class Location(models.Model):
+    name = models.CharField(max_length = 30)
+
+    def __str__(self):
+        return self.name
+
+class Category(models.Model):
+    name = models.CharField(max_length = 30)
+
+    def __str__(self):
+        return self.name
+
+class Image(models.Model):
+    #image =
+    name = models.CharField(max_length = 30)
+    description = models.CharField(max_length = 256)
+    category = models.ManyToManyField(Category)
+    location = models.ForeignKey(Location, default = True)
