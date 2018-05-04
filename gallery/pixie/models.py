c293217a2e4c465e53d46f0b2d1122d11f4ea992
rkemmy <rkemmy.rk69@gmail.com>
2018-05-04 15:18:39 +0300

    Create a method to query the database for images by category

--- gallery/pixie/models.py
+++ gallery/pixie/models.py
@@ -14,9 +14,24 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
+    @classmethod
+    def search_by_category(cls,search_term):
+        result = cls.objects.filter(name__icontains=search_term).first()
+        return result
+
 class Image(models.Model):
-    #image =
+    image = models.ImageField(upload_to = 'gallery/')
     name = models.CharField(max_length = 30)
     description = models.CharField(max_length = 256)
     category = models.ManyToManyField(Category)
-    location = models.ForeignKey(Location, default = True)
+    location = models.ForeignKey(Location)
+
+    @classmethod
+    def get_all(cls):
+        photo = cls.objects.all()
+        return photo
+
+    @classmethod
+    def get_image_by_category(cls,category):
+        img = cls.objects.filter(category = category).all()
+        return img
