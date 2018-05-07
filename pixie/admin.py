c9e974286a7117c241e84fd263174acd631e83d6
rkemmy <rkemmy.rk69@gmail.com>
2018-05-07 15:08:56 +0300

    Add category filter

--- pixie/admin.py
+++ pixie/admin.py
@@ -2,6 +2,10 @@ from django.contrib import admin
 from .models import Image,Location,Category
 
 # Register your models here.
-admin.site.register(Image)
+
+class ImageAdmin(admin.ModelAdmin):
+    filter_horizontal = ('category',)
+
+admin.site.register(Image,ImageAdmin)
 admin.site.register(Location)
 admin.site.register(Category)
