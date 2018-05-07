cd1fbba804ee9d34f07d220d1b5c56f313807b8b
rkemmy <rkemmy.rk69@gmail.com>
2018-05-07 16:23:32 +0300

    create a function to link to index url

--- pixie/urls.py
+++ pixie/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls.static import static
 from django.conf.urls import url
 from . import views
 urlpatterns=[
-    url('^$',views.index,name='welcome'),
+    url('^$',views.index,name='index'),
     url(r'^search/', views.search_results,name='search_results')
 ]
 if settings.DEBUG:
