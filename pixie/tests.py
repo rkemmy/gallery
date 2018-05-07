d4bc01a10c1fd8b6b5b6c90f848973dd78781eee
rkemmy <rkemmy.rk69@gmail.com>
2018-05-07 13:53:40 +0300

    create tests

--- pixie/tests.py
+++ pixie/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
+from .models import Location, Image, Category
 
 # Create your tests here.
+class ImageTestClass(TestCase):
+    def setUp(self):
+        self.location = Location(name='nairobi')
+        self.location.save()
+
+        self.category = Category(name ='food')
+        self.category.save()
+
+        self.image = Image( id = 1,name ='bg.jpg',description = 'good', category=self.category, location=self.location)
+
+
+
+    def test_save(self):
+        self.image
+        self.image.image_save()
+        self.assertTrue(len(Image.objects,all())>0)
